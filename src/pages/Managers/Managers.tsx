d109265e6ec7d3d31c31ea6b7bec5a855d51b2ba
tylergilbert50 <tylergilbert517@gmail.com>
2025-10-16 19:19:51 -0500

    Fix small bug

--- src/pages/Managers/Managers.tsx
+++ src/pages/Managers/Managers.tsx
@@ -8,13 +8,13 @@ function Managers() {
       <hr className="dashed-line" />
       <div className="manager-buttons-wrapper">
         <div className="manager-item">
-          <img src="/images/managers/andrew.avif" />
+          <img src="/images/managers/andrew.png" />
           <Link to="/andrew" className="manager-button">
             ANDREW
           </Link>
         </div>
         <div className="manager-item">
-          <img src="/images/managers/brett.avif" />
+          <img src="/images/managers/brett.png" />
           <Link to="/brett" className="manager-button">
             BRETT
           </Link>
@@ -26,43 +26,43 @@ function Managers() {
           </Link>
         </div>
         <div className="manager-item">
-          <img src="/images/managers/daniel.avif" />
+          <img src="/images/managers/daniel.png" />
           <Link to="/daniel" className="manager-button">
             DANIEL
           </Link>
         </div>
         <div className="manager-item">
-          <img src="/images/managers/danny.avif" />
+          <img src="/images/managers/danny.png" />
           <Link to="/danny" className="manager-button">
             DANNY
           </Link>
         </div>
         <div className="manager-item">
-          <img src="/images/managers/demarco.avif" />
+          <img src="/images/managers/demarco.png" />
           <Link to="/demarco" className="manager-button">
             DEMARCO
           </Link>
         </div>
         <div className="manager-item">
-          <img src="/images/managers/jeremy.avif" />
+          <img src="/images/managers/jeremy.png" />
           <Link to="/jeremy" className="manager-button">
             JEREMY
           </Link>
         </div>
         <div className="manager-item">
-          <img src="/images/managers/josh.avif" />
+          <img src="/images/managers/josh.png" />
           <Link to="/josh" className="manager-button">
             JOSH
           </Link>
         </div>
         <div className="manager-item">
-          <img src="/images/managers/stuart.avif" />
+          <img src="/images/managers/stuart.png" />
           <Link to="/stuart" className="manager-button">
             STUART
           </Link>
         </div>
         <div className="manager-item">
-          <img src="/images/managers/tyler.avif" />
+          <img src="/images/managers/tyler.png" />
           <Link to="/tyler" className="manager-button">
             TYLER
           </Link>
