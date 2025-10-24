f88009d9ef0e6634f0d8bca95bf527a995aa2583
tylergilbert50 <tylergilbert517@gmail.com>
2025-10-23 19:16:49 -0500

    Hot fix?

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwetzPY0lTLFw-OquhFTKzGpnVk4NJDjw",
-  authDomain: "avengers-fantasy-football.firebaseapp.com",
-  projectId: "avengers-fantasy-football",
-  storageBucket: "avengers-fantasy-football.firebasestorage.app",
-  messagingSenderId: "886621122361",
-  appId: "1:886621122361:web:89892956c1456277d402e8",
-  measurementId: "G-FCNN1FNPW1",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
